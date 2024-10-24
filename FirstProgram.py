159aa429d15e9da59dcc81d953ededb153592c03
kashyapanjali <anjalikashyap9608@gmail.com>
2024-10-24 13:41:19 +0530

    test

---
+++ FirstProgram.py
@@ -0,0 +1,22 @@
+print("Hello Anjali","How are you") 
+# num1 = int(input("num1:"))
+# num2 = int(input("num2:"))
+# sum = num1 + num2
+# print("sum is :", sum)
+
+# side = int(input("side of square:"))
+# print(side ** 2)
+
+# first = float(input("num1:"))
+# second = float(input("num2:"))
+
+# avg = (first + second) / 2
+# print(avg)
+
+val1 = int(input("first number:"))
+val2 = int(input("second number:"))
+def sum():
+    return val1 + val2
+
+val = sum()
+print(val)
